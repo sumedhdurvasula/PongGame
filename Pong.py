d2390b5c8fbeef75dd782974949400b4d693b706
sumedhdurvasula <144955207+sumedhdurvasula@users.noreply.github.com>
2023-09-27 15:42:53 -0400

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -5,13 +5,13 @@ from pygame.locals import *
 pygame.init()
 fps = pygame.time.Clock()
 
-#colors
+
 WHITE = (255,255,255)
 RED = (255,0,0)
 GREEN = (0,255,0)
 BLACK = (0,0,0)
 
-#globals
+
 WIDTH = 600
 HEIGHT = 400       
 BALL_RADIUS = 20
@@ -26,14 +26,13 @@ paddle2_vel = 0
 l_score = 0
 r_score = 0
 
-#canvas declaration
+
 window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 pygame.display.set_caption('Hello World')
 
-# helper function that spawns a ball, returns a position vector and a velocity vector
-# if right is True, spawn to the right, else spawn to the left
+
 def ball_init(right):
-    global ball_pos, ball_vel # these are vectors stored as lists
+    global ball_pos, ball_vel 
     ball_pos = [WIDTH//2,HEIGHT//2]
     horz = random.randrange(2,4)
     vert = random.randrange(1,3)
@@ -43,10 +42,10 @@ def ball_init(right):
         
     ball_vel = [horz,-vert]
 
-# define event handlers
+
 def init():
-    global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel,l_score,r_score  # these are floats
-    global score1, score2  # these are ints
+    global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel,l_score,r_score 
+    global score1, score2 
     paddle1_pos = [HALF_PAD_WIDTH - 1,HEIGHT//2]
     paddle2_pos = [WIDTH +1 - HALF_PAD_WIDTH,HEIGHT//2]
     l_score = 0
@@ -57,7 +56,7 @@ def init():
         ball_init(False)
 
 
-#draw function of canvas
+
 def draw(canvas):
     global paddle1_pos, paddle2_pos, ball_pos, ball_vel, l_score, r_score
            
@@ -67,7 +66,6 @@ def draw(canvas):
     pygame.draw.line(canvas, WHITE, [WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1)
     pygame.draw.circle(canvas, WHITE, [WIDTH//2, HEIGHT//2], 70, 1)
 
-    # update paddle's vertical position, keep paddle on the screen
     if paddle1_pos[1] > HALF_PAD_HEIGHT and paddle1_pos[1] < HEIGHT - HALF_PAD_HEIGHT:
         paddle1_pos[1] += paddle1_vel
     elif paddle1_pos[1] == HALF_PAD_HEIGHT and paddle1_vel > 0:
@@ -82,22 +80,22 @@ def draw(canvas):
     elif paddle2_pos[1] == HEIGHT - HALF_PAD_HEIGHT and paddle2_vel < 0:
         paddle2_pos[1] += paddle2_vel
 
-    #update ball
+
     ball_pos[0] += int(ball_vel[0])
     ball_pos[1] += int(ball_vel[1])
 
-    #draw paddles and ball
+
     pygame.draw.circle(canvas, RED, ball_pos, 20, 0)
     pygame.draw.polygon(canvas, GREEN, [[paddle1_pos[0] - HALF_PAD_WIDTH, paddle1_pos[1] - HALF_PAD_HEIGHT], [paddle1_pos[0] - HALF_PAD_WIDTH, paddle1_pos[1] + HALF_PAD_HEIGHT], [paddle1_pos[0] + HALF_PAD_WIDTH, paddle1_pos[1] + HALF_PAD_HEIGHT], [paddle1_pos[0] + HALF_PAD_WIDTH, paddle1_pos[1] - HALF_PAD_HEIGHT]], 0)
     pygame.draw.polygon(canvas, GREEN, [[paddle2_pos[0] - HALF_PAD_WIDTH, paddle2_pos[1] - HALF_PAD_HEIGHT], [paddle2_pos[0] - HALF_PAD_WIDTH, paddle2_pos[1] + HALF_PAD_HEIGHT], [paddle2_pos[0] + HALF_PAD_WIDTH, paddle2_pos[1] + HALF_PAD_HEIGHT], [paddle2_pos[0] + HALF_PAD_WIDTH, paddle2_pos[1] - HALF_PAD_HEIGHT]], 0)
 
-    #ball collision check on top and bottom walls
+
     if int(ball_pos[1]) <= BALL_RADIUS:
         ball_vel[1] = - ball_vel[1]
     if int(ball_pos[1]) >= HEIGHT + 1 - BALL_RADIUS:
         ball_vel[1] = -ball_vel[1]
     
-    #ball collison check on gutters or paddles
+
     if int(ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH and int(ball_pos[1]) in range(paddle1_pos[1] - HALF_PAD_HEIGHT,paddle1_pos[1] + HALF_PAD_HEIGHT,1):
         ball_vel[0] = -ball_vel[0]
         ball_vel[0] *= 1.1
@@ -114,7 +112,7 @@ def draw(canvas):
         l_score += 1
         ball_init(False)
 
-    #update scores
+
     myfont1 = pygame.font.SysFont("Comic Sans MS", 20)
     label1 = myfont1.render("Score "+str(l_score), 1, (255,255,0))
     canvas.blit(label1, (50,20))
@@ -123,8 +121,7 @@ def draw(canvas):
     label2 = myfont2.render("Score "+str(r_score), 1, (255,255,0))
     canvas.blit(label2, (470, 20))  
     
-    
-#keydown handler
+
 def keydown(event):
     global paddle1_vel, paddle2_vel
     
@@ -137,7 +134,7 @@ def keydown(event):
     elif event.key == K_s:
         paddle1_vel = 8
 
-#keyup handler
+
 def keyup(event):
     global paddle1_vel, paddle2_vel
     
@@ -149,7 +146,7 @@ def keyup(event):
 init()
 
 
-#game loop
+
 while True:
 
     draw(window)
